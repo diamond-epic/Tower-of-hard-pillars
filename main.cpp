22eaa12661efed8ef8ed9f8dea7dc443acd0d94d
Diamond Epic <xxdiamondepicxx1@gmail.com>
2026-05-01 22:02:13 -0300

    moosic

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include <string>
+#include <iostream> // used for printing (cout) and input (cin)
+#include <windows.h> // for window management i think
+#include <string> // pretty self explanatory i hope
+#include <cmath> // did you know that cmath actually stands for complex math? actually idk if this is true don't quote me on this lmao
+#include <vector> // resizable arrays
+#include <fstream> // file reading
+
+// this'll be defined later don't worry
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+// stuff that runs when the program starts
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+    // Register the window class.
+    const wchar_t CLASS_NAME[]  = L"Tower of hard pillars";
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    return 0;
+}
